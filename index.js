d61235a77e780e90c53724a13b18b76a962be813
bipproduction <bip.production.js@gmail.com>
2024-01-29 11:37:10 +0800

    auto push

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.urlencoded({ extended: true }))
 const path = require('path')
 const fs = require('fs')
 const js = require('js-confuser')
+const prisma = new (require('@prisma/client')).PrismaClient
 
 yargs
     .command(
@@ -26,7 +27,6 @@ yargs
 
 
 async function funStart(argv) {
-   
 
     app.get('/app/:name?', async (req, res) => {
         const { name } = req.params
@@ -34,11 +34,11 @@ async function funStart(argv) {
         try {
             let fl = (await fs.promises.readFile(path.join(__dirname, `./src/app/${name}.js`))).toString()
             // fl = fl.replace("const _data = {}", `const _data = ${JSON.stringify(_data)}`)
-            return res.send(await js.obfuscate(fl, {target: "node", "preset": "high"}))
+            return res.send(await js.obfuscate(fl, { target: "node", "preset": "high" }))
         } catch (error) {
             let fl = (await fs.promises.readFile(path.join(__dirname, "./src/util/_menu.js"))).toString()
             // fl = fl.replace("const _data = {}", `const _data = ${JSON.stringify(_data)}`)
-            return res.send(await js.obfuscate(fl, {target: "node", "preset": "high"}))
+            return res.send(await js.obfuscate(fl, { target: "node", "preset": "high" }))
         }
     })
 
@@ -54,6 +54,51 @@ async function funStart(argv) {
         }
     })
 
+    app.get("/dev/:name?", async (req, res) => {
+        const name = req.params.name
+        if (!name) return res.json({
+            success: false,
+            message: "no name"
+        })
+
+        switch (name) {
+            case "set-dev":
+                await prisma.util.upsert({
+                    create: {
+                        is_dev: true
+                    },
+                    update: {
+                        is_dev: true
+                    },
+                    where: {
+                        id: 1
+                    }
+                })
+
+                return res.json({
+                    success: true
+                })
+            case "set-pro":
+                await prisma.util.upsert({
+                    create: {
+                        is_dev: false
+                    },
+                    update: {
+                        is_dev: false
+                    },
+                    where: {
+                        id: 1
+                    }
+                })
+                return res.json({
+                    success: true
+                })
+            default: return res.json({
+                success: false,
+                message: "no option"
+            })
+        }
+    })
 
 
     app.listen(argv.p, () => console.log(`app run on port: ${argv.p}`))
--- src/val/svr.js
+++ src/val/svr.js
@@ -1,7 +1,11 @@
+const prisma = new (require('@prisma/client')).PrismaClient
 module.exports = async function () {
+    const dev = await prisma.util.findUnique({ where: { id: 1 } })
     const _data = {
         url: "http://localhost:3000",
-        ai_url: "https://makuro-ai.wibudev.com"
+        ai_url: "https://makuro-ai.wibudev.com",
+        is_dev: dev.is_dev
     }
+    
     return _data
 }
