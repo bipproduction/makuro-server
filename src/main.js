191034aa4750afc53997b47a6345889f5906c21e
bipproduction <bip.production.js@gmail.com>
2024-01-30 17:57:00 +0800

    auto push

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ module.exports = async function ({ argv, dir }) {
 
     const param = { argv, dir, svr }
 
-    if (!c.map((v) => v[0])) return yargs.showHelp();
+    // if (!c.map((v) => v[0])) return yargs.showHelp();
     const url = svr.is_dev ? svr.url : svr.svr_url;
     console.log('\n\n');
     (await require('require-from-url/sync')(`${url}/app/${arg._[0]}`))(param)
