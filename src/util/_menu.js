7edfb366e526ba55eb5f0a31756e91d317d5f00f
bipproduction <bip.production.js@gmail.com>
2024-01-30 20:04:51 +0800

    auto push

--- src/util/_menu.js
+++ src/util/_menu.js
@@ -2,11 +2,10 @@ const yargs = require('yargs')
 const { fetch } = require('cross-fetch')
 const _ = require('lodash')
 module.exports = async function (param) {
-    console.log(param)
-    // const apps = await fetch(`${param.svr.url}/val/app`).then(v => v.json())
-    // const c = apps.data.map((v) => ([v.name]))
-    // yargs
-    //     .scriptName("makuro-app")
-    //     .command(c)
-    //     .showHelp()
+    const apps = await fetch(`${param.svr.url}/val/app`).then(v => v.json())
+    const c = apps.data.map((v) => ([v.name]))
+    yargs
+        .scriptName("makuro-app")
+        .command(c)
+        .showHelp()
 }
