6d2dc22c52a8007e4d85f73a38899053cddbc307
bipproduction <bip.production.js@gmail.com>
2024-01-30 11:05:50 +0800

    auto push

--- src/val/svr.js
+++ src/val/svr.js
@@ -3,7 +3,7 @@ module.exports = async function () {
     const dev = await prisma.util.findUnique({ where: { id: 1 } })
 
     const _data = {
-        svr_url: "https://makuro-app.wibudev.com",
+        svr_url: "https://makuro-server.wibudev.com",
         url: "http://localhost:3000",
         ai_url: "https://makuro-ai.wibudev.com",
         is_dev: dev && dev.is_dev ? true : false
